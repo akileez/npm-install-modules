1238f534c2f33d93e09f758f39040088bde1055d
akileez <git@keeda.net>
2015-08-03 02:48:18 -0400

    source file index.js

--- index.js
+++ index.js
@@ -7,6 +7,52 @@
 
 'use strict'
 
-module.exports = function () {
-  // do stuff
+var exec       = require('child_process').exec
+var assert     = require('assert')
+var concurrent = require('./lib/concurrent')
+var logger     = require('./lib/logger')
+var isEmpty    = require('./lib/isEmpty')
+
+function install (opts, done) {
+  logger.info('\nInstalling Dependencies:\n\n')
+
+  function installDependencies (cmd, packages, msg, cb) {
+    if (!isEmpty(packages)) {
+      concurrent.each(packages, function (val, key, done) {
+        exec(cmd + val, function (err) {
+          logger.event('  module: ', val)
+          done(null, msg)
+        })
+      }, function (err) {
+        assert.ifError(err)
+        cb(null, msg)
+      })
+    } else {
+      cb(null, msg)
+    }
+  }
+
+  var commands = {
+    cmd1: function (cb) {
+      var cmd = 'npm install --save-dev '
+      installDependencies(cmd, opts.devDependencies, 'devDependencies', function (err, msg) {
+        cb(null, msg)
+      })
+    },
+
+    cmd2: function (cb) {
+      var cmd = 'npm install --save '
+      installDependencies(cmd, opts.dependencies, 'dependencies', function (err, msg) {
+        cb(null, msg)
+      })
+    }
+  }
+
+  concurrent.parallel(commands, function (err, results) {
+    assert.ifError(err)
+    logger.msgdone()
+    done()
+  })
 }
+
+module.exports = install
